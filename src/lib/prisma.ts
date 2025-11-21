385e6cf2575fab903d99add666d4581aae89f756
HPVIRUS <hpvirus89@gmail.com>
2025-11-21 16:22:00 +0330

    Delete src directory

--- src/lib/prisma.ts
+++
@@ -1,6 +0,0 @@
-
-import { PrismaClient } from '@prisma/client'
-import type * as PrismaType from '@prisma/client';
-
-export const prisma = new PrismaClient();
-export type { PrismaType };
--- src/type/index.ts
+++
@@ -1,5 +0,0 @@
-import { Prisma } from "@prisma/client";
-
-export type ProductsWithImage = Prisma.ProductGetPayload<{
-  include: { image: true };
-}>;
